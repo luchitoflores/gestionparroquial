f31e3d2400cc63e2e2ea93b753e735721b14ac7e
luchitoflores <luchitoflores84@gmail.com>
2014-01-08 17:43:13 -0500

    modal email para asignar secretaria

--- sacramentos/forms.py
+++ sacramentos/forms.py
@@ -1160,18 +1160,16 @@ class AsignarSecretariaForm(ModelForm):
 		cleaned_data = super(AsignarSecretariaForm, self).clean()
 		persona = cleaned_data.get("persona")
 		parroquia = cleaned_data.get("parroquia")
-
+		esta_activo_otra_parroquia= PeriodoAsignacionParroquia.objects.filter(asignacion__persona=persona, estado=True).exclude(asignacion__parroquia=parroquia)
 
 		if not persona.user.email:
 			mensaje = u"La persona elegida ya tiene una asignación activa en otra parroquia"
 			msg = mark_safe(u"%s %s" % ('<a href="">Prueba</a>', mensaje))
 			self._errors["persona"] = self.error_class([msg])
 
-		elif:
-			esta_activo_otra_parroquia= PeriodoAsignacionParroquia.objects.filter(asignacion__persona=persona, estado=True).exclude(asignacion__parroquia=parroquia)
-			if esta_activo_otra_parroquia:
-				msg = u"La persona elegida ya tiene una asignación activa en otra parroquia"
-				self._errors["persona"] = self.error_class([msg])
+		elif esta_activo_otra_parroquia:
+			msg = u"La persona elegida ya tiene una asignación activa en otra parroquia"
+			self._errors["persona"] = self.error_class([msg])
 
 		return cleaned_data
 
