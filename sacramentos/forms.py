42f8bd109d2a7b62bc930bda343b13ff62f17c44
luchitoflores <luchitoflores84@gmail.com>
2014-01-08 15:54:29 -0500

    modal email para asignar secretaria

--- sacramentos/forms.py
+++ sacramentos/forms.py
@@ -163,7 +163,7 @@ class UsuarioAdministradorForm(ModelForm):
 	last_name = forms.CharField(required=True, label='Apellidos *', 
 		help_text='Ingrese los nombres completos. Ej: Castro Pardo',
 		widget=forms.TextInput(attrs={'required': ''}))
-	groups = forms.ModelMultipleChoiceField(required=False, label='Grupos *', queryset= Group.objects.all(),
+	groups = forms.ModelMultipleChoiceField(required=False, label='Grupos *', queryset= Group.objects.all().order_by('name'),
 		help_text = 'Los grupos a los que este usuario pertenece. '+
 		'Un usuario obtendrá todos los permisos concedidos a cada uno sus grupos.'+
 		' Ud. puede seleccionar más de una opción.', widget=forms.CheckboxSelectMultiple())
