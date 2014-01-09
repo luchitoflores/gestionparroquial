0b947a86f7ba1c13aee8b3a0f39829cdb4fa532d
luchitoflores <luchitoflores84@gmail.com>
2014-01-09 16:10:24 -0500

    modificación a la tabla de matrimonios

--- ciudades/admin.py
+++ ciudades/admin.py
@@ -7,8 +7,9 @@ from .models import Provincia, Canton,Parroquia, Direccion
 	
 
 class ParroquiasAdmin(admin.ModelAdmin):
-	list_display = ('nombre', 'canton',)
+	list_filter = ('nombre',)
 
+	
 admin.site.register(Provincia)
 admin.site.register(Canton)
 admin.site.register(Parroquia, ParroquiasAdmin)
