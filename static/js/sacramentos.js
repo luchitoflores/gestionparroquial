c0a721e01833881092dcf8f1d363858b9c102541
luchitoflores <luchitoflores84@gmail.com>
2014-01-23 11:59:24 -0500

    url para accesibilidad

--- static/js/sacramentos.js
+++ static/js/sacramentos.js
@@ -3,17 +3,6 @@ document.write('<script src="/static/js/tablas.js" type="text/javascript"></scri
 
 function inicio(){
 
-	if (BrowserDetect.browser == "Explorer"){ 
-		if (BrowserDetect.version<9){
-			alert('Se recomienda utilizar Chrome, Firefox u Opera para el ingreso al sistema');
-		}
-	} else if (BrowserDetect.browser == "Safari"){
-		alert('Se recomienda utilizar Chrome, Firefox u Opera para el ingreso al sistema');
-	}
-
-
-
-
 	if(localStorage.nombre){
 		if(localStorage.getItem('nombre')=='pequenia'){
 			$('body').css('font-size', '1em');
@@ -1118,6 +1107,124 @@ function prueba_localstore(){
 		$('body').css('font-size', '1.5em');
 	});
 }
+
+function detectar_navegador(){
+	
+	var BrowserDetect = { 
+		init: function () { 
+			this.browser = this.searchString(this.dataBrowser) || "An unknown browser"; 
+			this.version = this.searchVersion(navigator.userAgent) 
+			|| this.searchVersion(navigator.appVersion) 
+			|| "an unknown version"; 
+			this.OS = this.searchString(this.dataOS) || "an unknown OS"; 
+		}, 
+		searchString: function (data) { 
+			for (var i=0;i<data.length;i++)	{ 
+				var dataString = data[i].string; 
+				var dataProp = data[i].prop; 
+				this.versionSearchString = data[i].versionSearch || data[i].identity; 
+				if (dataString) { 
+					if (dataString.indexOf(data[i].subString) != -1) 
+						return data[i].identity; 
+				} 
+				else if (dataProp) 
+					return data[i].identity; 
+			} 
+		}, 
+		searchVersion: function (dataString) { 
+			var index = dataString.indexOf(this.versionSearchString); 
+			if (index == -1) return; 
+			return parseFloat(dataString.substring(index+this.versionSearchString.length+1)); 
+		}, 
+		dataBrowser: [ 
+		{ string: navigator.userAgent, 
+			subString: "OmniWeb", 
+			versionSearch: "OmniWeb/", 
+			identity: "OmniWeb" 
+		}, 
+		{ 
+			string: navigator.vendor, 
+			subString: "Apple", 
+			identity: "Safari" 
+		}, 
+		{ 
+			prop: window.opera, 
+			identity: "Opera" 
+		}, 
+		{ 
+			string: navigator.vendor, 
+			subString: "iCab", 
+			identity: "iCab" 
+		}, 
+		{ 
+			string: navigator.vendor, 
+			subString: "KDE", 
+			identity: "Konqueror" 
+		}, 
+		{ 
+			string: navigator.userAgent, 
+			subString: "Firefox", 
+			identity: "Firefox" 
+		}, 
+		{ 
+			string: navigator.vendor, 
+			subString: "Camino", 
+			identity: "Camino" 
+		}, 
+   	 {	 // for newer Netscapes (6+) 
+   	 	string: navigator.userAgent, 
+   	 	subString: "Netscape", 
+   	 	identity: "Netscape" 
+   	 }, 
+   	 { 
+   	 	string: navigator.userAgent, 
+   	 	subString: "MSIE", 
+   	 	identity: "Explorer", 
+   	 	versionSearch: "MSIE" 
+   	 }, 
+   	 { 
+   	 	string: navigator.userAgent, 
+   	 	subString: "Gecko", 
+   	 	identity: "Mozilla", 
+   	 	versionSearch: "rv" 
+   	 }, 
+   	 { // for older Netscapes (4-) 
+   	 	string: navigator.userAgent, 
+   	 	subString: "Mozilla", 
+   	 	identity: "Netscape", 
+   	 	versionSearch: "Mozilla" 
+   	 } 
+   	 ], 
+   	 dataOS : [ 
+   	 { 
+   	 	string: navigator.platform, 
+   	 	subString: "Win", 
+   	 	identity: "Windows" 
+   	 }, 
+   	 { 
+   	 	string: navigator.platform, 
+   	 	subString: "Mac", 
+   	 	identity: "Mac" 
+   	 }, 
+   	 { 
+   	 	string: navigator.platform, 
+   	 	subString: "Linux", 
+   	 	identity: "Linux" 
+   	 } 
+   	 ] 
+
+   	}; 
+   	BrowserDetect.init(); 
+
+   	if (BrowserDetect.browser == "Explorer"){ 
+   		if (BrowserDetect.version<9){
+   			alert('Se recomienda utilizar Chrome, Firefox u Opera para el ingreso al sistema');
+   		}
+   	} else if (BrowserDetect.browser == "Safari"){
+   		alert('Se recomienda utilizar Chrome, Firefox u Opera para el ingreso al sistema');
+   	}
+   	
+   }
 
 
 
