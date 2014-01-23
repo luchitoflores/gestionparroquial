2f9fb1936cefe4a6cc10619f0a7d1c6bdd83b560
luchitoflores <luchitoflores84@gmail.com>
2014-01-23 11:16:18 -0500

    url para accesibilidad

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,7 @@ from .views import(
 urlpatterns = patterns('',
 	url(r'^$',TemplateView.as_view(template_name='index.html'), name='index'),
 	url(r'^home/$', login_required(TemplateView.as_view(template_name='home.html'),login_url='/login/'), name='home'),   
+	url(r'^accesibilidad/$', login_required(TemplateView.as_view(template_name='accesibilidad.html'),login_url='/login/'), name='accesibilidad'),   
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^buscar/$', buscar_sacramentos_view, name='buscar'),   
 )
