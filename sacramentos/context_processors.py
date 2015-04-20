024c1cf05e0f66d66d4dc18933ed212d6b0ff983
Lucho <luchitoflores84@gmail.com>
2015-04-20 17:10:19 -0500

    Se modifican los fixtures del core

--- sacramentos/context_processors.py
+++ sacramentos/context_processors.py
@@ -3,29 +3,19 @@ from django.core.urlresolvers import reverse_lazy
 
 from .models import ParametrizaDiocesis, ParametrizaParroquia
 
+
 def parametros_diocesis(request):
-	try:
-		parametros = ParametrizaDiocesis.objects.get(pk=1)
-		return {'parametros': parametros}
-	except ObjectDoesNotExist:
-		return {'parametros': ''}
-	
+    try:
+        parametros = ParametrizaDiocesis.objects.get(pk=1)
+        return {'parametros': parametros}
+    except ObjectDoesNotExist:
+        return {'parametros': ''}
+
+
 def parametros_parroquiales(request):
-	parametros = ParametrizaParroquia.objects.all()
-	if parametros:
-		return {'parametros_parroquia': parametros[0]}
-	else:
-		return {'parametros_parroquia': ''}
+    parametros = ParametrizaParroquia.objects.all()
+    if parametros:
+        return {'parametros_parroquia': parametros[0]}
+    else:
+        return {'parametros_parroquia': ''}
 
-def menu(request):
-	menu = {'menu': [
-	{'nombre': 'Iglesia', 'url': reverse_lazy('iglesia_list')},
-	{'nombre': 'Ciudades', 'submenu': [
-		{'nombre': 'Provincias','url':reverse_lazy('provincia_list')},
-		{'nombre': 'Cantones','url':reverse_lazy('canton_list')},
-		{'nombre': 'Parroquias','url':reverse_lazy('parroquia_list')},
-		]},
-	{'nombre': 'Intenciones', 'url': reverse_lazy('intencion_list')},
-	]}
-
-	return menu
