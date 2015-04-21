e64f04212b040a2f37bc273dce2086b02440839f
Lucho <luchitoflores84@gmail.com>
2015-04-21 13:02:25 -0500

    Se cambia el diseño del login
    Se cambia el diseño del login

--- sacramentos/templatetags/tags_html.py
+++ sacramentos/templatetags/tags_html.py
@@ -1,20 +1,25 @@
 # -*- coding:utf-8 -*-
 from django import template
-from sacramentos.forms import UsuarioForm, PadreForm,PerfilUsuarioForm
+from sacramentos.forms import UsuarioForm, PadreForm, PerfilUsuarioForm
 from sacramentos.models import ParametrizaDiocesis, Libro
-from sacramentos.views import  LibroForm
+from sacramentos.views import LibroForm
+
 register = template.Library()
+
 
 @register.filter(is_safe=True)
 def label_with_classes(value, arg):
     return value.label_tag(attrs={'class': arg})
 
+
 @register.simple_tag
 def footer_tag():
-	objeto = ParametrizaDiocesis.objects.all()
-		
-	if objeto[0]:
-		return u'<h5>%s</h5><p>%s | %s - Ecuador | Teléfono: %s </p>' % (objeto[0].diocesis, objeto[0].direccion, str(objeto[0].direccion.provincia).title(), objeto[0].direccion.telefono)
-	else:
-		return ""
+    objeto = ParametrizaDiocesis.objects.all()
+
+    if objeto[0]:
+        return u'<h5>%s</h5><p>%s | %s - Ecuador | Teléfono: %s </p>' % (
+        objeto[0].diocesis, objeto[0].direccion, str(objeto[0].direccion.provincia).title(),
+        objeto[0].direccion.telefono)
+    else:
+        return ""
 
